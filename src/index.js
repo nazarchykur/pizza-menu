5f73b7db26991f7875f642f0b5e8afb6aeaf3c96
nazarchykur <nazarchykur@gmail.com>
2023-09-11 15:49:01 +0300

    added an example of React Fragments

--- src/index.js
+++ src/index.js
@@ -82,11 +82,18 @@ function Menu() {
       <h2> Our Menu</h2>
 
       {pizzasNumber > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>"We're stiil working on our menu. Please come back later:)"</p>
       )}
